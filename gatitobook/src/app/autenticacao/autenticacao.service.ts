a05e2ecceacab6763026861355fa036d1a8bbf4b
welligton_fadami <welligtoncos@gmail.com>
2022-12-29 18:29:34 -0300

    projeto inicial gatitobook angular

---
+++ gatitobook/src/app/autenticacao/autenticacao.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UsuarioService } from './usuario/usuario.service';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AutenticacaoService {
+  constructor(
+    private httpClient: HttpClient,
+    private usuarioService: UsuarioService
+  ) {}
+ 
+  autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
+    
+    return this.httpClient
+      .post(
+        'http://localhost:6000/login',
+        {
+          Username: usuario,
+          Password: senha,
+        },
+        { observe: 'response' }
+      )
+      .pipe(
+        tap((res) => {
+          const authToken = res.body;
+          console.log(authToken)
+        })
+      );
+  }
+}
