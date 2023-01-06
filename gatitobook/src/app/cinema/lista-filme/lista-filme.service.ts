95b85b86d72cfc5ce6b440a8539adbb222f53778
welligton_fadami <welligtoncos@gmail.com>
2023-01-05 23:32:43 -0300

    Adicionando async filme list

--- gatitobook/src/app/cinema/lista-filme/lista-filme.service.ts
+++ gatitobook/src/app/cinema/lista-filme/lista-filme.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { TokenService } from 'src/app/autenticacao/token.service';
 import { Filme } from '../novo-filme/Filme';
 
@@ -18,4 +19,14 @@ export class ListaFilmeService {
     return this.http.get<Filme[]>('http://localhost:5000/filme',{headers:head_obj2}); 
    
   }
+
+  remove(id: Int16Array){
+    const token = JSON.parse(this.tokenService.retornaToken());  
+    let head_obj2= new HttpHeaders().set("Authorization","bearer "+token)
+    return this.http.delete('http://localhost:5000/filme/'+id,{headers:head_obj2}).pipe(take(1));
+  }
+
+   
+
+
 }
