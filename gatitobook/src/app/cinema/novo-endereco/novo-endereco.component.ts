24f19abd6cb1e1b109333d524777a4c06e77cd6e
welligton_fadami <welligtoncos@gmail.com>
2023-01-21 17:29:22 -0300

    interligacao de service e ts novo-endereco

--- gatitobook/src/app/cinema/novo-endereco/novo-endereco.component.ts
+++ gatitobook/src/app/cinema/novo-endereco/novo-endereco.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { AlertService } from 'src/app/shared/alert.service';
+import { AlertService, AlertTypes } from 'src/app/shared/alert.service';
 import { Endereco } from './Endereco';
+import { NovoEnderecoService } from './novo-endereco.service';
 
 @Component({
   selector: 'app-novo-endereco',
@@ -13,23 +14,39 @@ import { Endereco } from './Endereco';
 export class NovoEnderecoComponent implements OnInit {
 
   formularioEndereco!: FormGroup;
+
   constructor(
     private formBuilder: FormBuilder,
     private router: Router, 
+    private novoEnderecoService: NovoEnderecoService, 
     private spinner: NgxSpinnerService,
     private alertService: AlertService) { }
 
   ngOnInit(): void {
     this.formularioEndereco = this.formBuilder.group({
-      nome: ['', [Validators.required]]    
+      logradouro: ['', [Validators.required]] ,
+      bairro: ['', [Validators.required]] ,
+      numero: ['', [Validators.required]]    
     })
   }
 
   cadastrar() {
+    console.error("ERRO")
     this.spinner.show();
-    if(this.formularioEndereco.valid){ 
-      const novoFilme = this.formularioEndereco.getRawValue() as Endereco;
-      
+    if(this.formularioEndereco.valid){  
+      const novoEndereco = this.formularioEndereco.getRawValue() as Endereco;
+      this.novoEnderecoService.cadastraNovoEndereco(novoEndereco).subscribe(
+        () => {
+          this.alertService.showAlert("Filme cadastrado com sucesso!",AlertTypes.SUCCESS)
+          this.spinner.hide(); 
+          this.formularioEndereco.reset()
+        },
+        (error:any) => {
+          console.log(error);
+          this.alertService.showAlert("Endereço não cadastrado!",AlertTypes.DANGER)
+          this.spinner.hide()
+        }
+      );
     }
   }
 }
