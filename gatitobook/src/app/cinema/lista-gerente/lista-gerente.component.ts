36b64e88ed2a32acbd34a23d4361ae2384fcfdca
welligton_fadami <welligtoncos@gmail.com>
2023-01-20 21:45:12 -0300

    list gerente get

--- gatitobook/src/app/cinema/lista-gerente/lista-gerente.component.ts
+++ gatitobook/src/app/cinema/lista-gerente/lista-gerente.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { Observable } from 'rxjs';
+import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
+import { AlertService } from 'src/app/shared/alert.service';
+import { Gerente } from '../novo-gerente/Gerente';
+import { ListaGerenteService } from './lista-gerente.service';
 
 @Component({
   selector: 'app-lista-gerente',
@@ -6,10 +13,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-gerente.component.css']
 })
 export class ListaGerenteComponent implements OnInit {
+  Gerente$: Observable<Gerente[]> | undefined 
 
-  constructor() { }
+  constructor(
+    private listaGerenteService: ListaGerenteService,
+    private alertService: AlertService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private modalService:BsModalService,
+    private usuarioService: UsuarioService) {  
+    }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
+      this.Gerente$ = this.listaGerenteService.retornaGerentes(); 
   }
+   
 
 }
--- gatitobook/src/app/cinema/lista-gerente/lista-gerente.service.ts
+++ gatitobook/src/app/cinema/lista-gerente/lista-gerente.service.ts
@@ -1,14 +1,25 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { TokenService } from 'src/app/autenticacao/token.service';
+import { environment } from 'src/environments/environment';
+import { Gerente } from '../novo-gerente/Gerente';
+
+const API_URL_FILMES = environment.API_URL_FILMES;
 
 @Injectable({
   providedIn: 'root'
 })
-export class ListaGerenteService {
+export class ListaGerenteService { 
 
   constructor( 
     private http: HttpClient,private tokenService: TokenService) { }
 
  
+  retornaGerentes():Observable<Gerente[]>{
+    const token = JSON.parse(this.tokenService.retornaToken());  
+    let head_obj2= new HttpHeaders().set("Authorization","bearer "+token)
+    return this.http.get<Gerente[]>(`${API_URL_FILMES}/gerente`,{headers:head_obj2}); 
+   
+  }
 }
