0aab7b23d7a55d041dd143fb9bb51ba362558123
welligton_fadami <welligtoncos@gmail.com>
2023-01-21 23:12:52 -0300

    component novo cinema

---
+++ gatitobook/src/app/cinema/novo-cinema/Cinema.ts
@@ -0,0 +1,5 @@
+export interface Cinema { 
+    Nome:string | any;
+    EnderecoId:string | any;
+    GerenteId:Int16Array | any;
+}
--- gatitobook/src/app/cinema/novo-cinema/novo-cinema.component.ts
+++ gatitobook/src/app/cinema/novo-cinema/novo-cinema.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { AlertService } from 'src/app/shared/alert.service';
+import { Cinema } from './Cinema';
 
 @Component({
   selector: 'app-novo-cinema',
@@ -7,9 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovoCinemaComponent implements OnInit {
 
-  constructor() { }
+  formularioCinema!: FormGroup;
 
-  ngOnInit(): void {
-  }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router, 
+    //private novoEnderecoService: NovoEnderecoService, 
+    private spinner: NgxSpinnerService,
+    private alertService: AlertService) { }
+
+    ngOnInit(): void {
+      this.formularioCinema = this.formBuilder.group({
+        Nome: ['', [Validators.required]] ,
+        EnderecoId: ['', [Validators.required]] ,
+        GerenteId: ['', [Validators.required]]    
+      })
+    }
+
+    cadastrar() {  
+      console.error("")
+      console.log(this.formularioCinema.getRawValue())
+      this.spinner.show();
+      if(this.formularioCinema.valid){  
+        const novoCinema = this.formularioCinema.getRawValue() as Cinema; 
+      }
+    }
 
 }
