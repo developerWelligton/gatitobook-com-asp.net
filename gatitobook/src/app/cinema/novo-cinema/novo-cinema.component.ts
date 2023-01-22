27eba2fef673fca95b228a13ae103bdd23888527
welligton_fadami <welligtoncos@gmail.com>
2023-01-21 23:38:33 -0300

    option select endereco list in cinema

--- gatitobook/src/app/cinema/novo-cinema/novo-cinema.component.ts
+++ gatitobook/src/app/cinema/novo-cinema/novo-cinema.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { Observable } from 'rxjs';
 import { AlertService } from 'src/app/shared/alert.service';
+import { ListaEnderecoService } from '../lista-endereco/lista-endereco.service';
+import { Endereco } from '../novo-endereco/Endereco';
 import { Cinema } from './Cinema';
 
 @Component({
@@ -13,20 +16,21 @@ import { Cinema } from './Cinema';
 export class NovoCinemaComponent implements OnInit {
 
   formularioCinema!: FormGroup;
-
+  endereco$: Observable<Endereco[]> | undefined  
   constructor(
     private formBuilder: FormBuilder,
     private router: Router, 
-    //private novoEnderecoService: NovoEnderecoService, 
+    private listaEnderecoService: ListaEnderecoService, 
     private spinner: NgxSpinnerService,
     private alertService: AlertService) { }
 
     ngOnInit(): void {
+      this.endereco$ = this.listaEnderecoService.retornaEnderecos(); 
       this.formularioCinema = this.formBuilder.group({
         Nome: ['', [Validators.required]] ,
         EnderecoId: ['', [Validators.required]] ,
         GerenteId: ['', [Validators.required]]    
-      })
+      }) 
     }
 
     cadastrar() {  
@@ -36,6 +40,9 @@ export class NovoCinemaComponent implements OnInit {
       if(this.formularioCinema.valid){  
         const novoCinema = this.formularioCinema.getRawValue() as Cinema; 
       }
+    }
+    changeEndereco(event:any) {
+      console.log(event.target.value);
     }
 
 }
