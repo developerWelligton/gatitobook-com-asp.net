dfe35602ff761c171c153c37d7d56566192ebc14
welligton_fadami <welligtoncos@gmail.com>
2023-02-05 01:02:13 -0300

    ajust: reload grafico

--- gatitobook/src/app/cinema/grafico-filme/grafico-filme.component.ts
+++ gatitobook/src/app/cinema/grafico-filme/grafico-filme.component.ts
@@ -13,6 +13,7 @@ import { map } from 'rxjs/internal/operators/map';
   styleUrls: ['./grafico-filme.component.css']
 })
 export class GraficoFilmeComponent implements OnInit {
+  
   name = 'ng-chartjs ' + VERSION.major;;
   ingressosQuantidade$: Observable<Ingressos[]> | undefined  
   
@@ -75,15 +76,9 @@ export class GraficoFilmeComponent implements OnInit {
   
  
   ngOnInit() { 
-    
-    this.ingressosQuantidade$ = this.listaSessaoService.retornaIngressoQuantidade();
-    const ingressos = this.ingressosQuantidade$.subscribe((ingressos) => { 
-      ingressos.forEach(ingresso => {   
-        this.lineChartData[0].data.push(ingresso.total);
-        this.lineChartLabels.push(ingresso.sessaoId.toString());
-      });
-    });
- 
+    this.performAutoClick();
+  
+
     this.textPlugin = [{
       id: 'textPlugin',
       beforeDraw(chart: any): any {
@@ -94,7 +89,7 @@ export class GraficoFilmeComponent implements OnInit {
         const fontSize = (height / 160).toFixed(2);
         ctx.font = `${fontSize}em sans-serif`;
         ctx.textBaseline = 'middle';
-        const text = 'Ingresso X Sessão';
+        const text = '';
         const textX = Math.round((width - ctx.measureText(text).width) / 2);
         const textY = height / 2;
         ctx.fillText(text, textX, textY);
@@ -102,10 +97,44 @@ export class GraficoFilmeComponent implements OnInit {
       }
     }];
 
-    this.inlinePlugin = this.textPlugin;
+    this.inlinePlugin = this.textPlugin
+    
+    this.ingressosQuantidade$ = this.listaSessaoService.retornaIngressoQuantidade();
+    const ingressos = this.ingressosQuantidade$.subscribe((ingressos) => { 
+      ingressos.forEach(ingresso => {   
+        this.lineChartData[0].data.push(ingresso.total);
+        this.lineChartLabels.push(ingresso.sessaoId.toString());
+      });
+    }); 
+   
+  }  
+
+  chartClicked(e: any): void {
+    console.log('click', e);
   }
-  // events
- 
+
+  chartHovered(e: any): void {
+    console.log('hover', e);
+  }
+
+  changeChartType() {
+    if (this.lineChartType === 'line') {
+      this.lineChartType = 'bar';
+    } else {
+      this.lineChartType = 'line';
+    }
+  }
+
+  updateGetChartInstance() {
+    const chart: any = this.ngChartjsService.getChart('testChart'); 
+  }
   
+  performAutoClick() {
+    setTimeout(() => {
+      this.changeChartType();
+    }, 100);
+  }
+  
+
   
 }
